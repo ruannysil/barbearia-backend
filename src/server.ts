e41035d6ac481cc8f2e945d941412386b08f83e6
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-06-22 08:30:55 -0300

    update backend

--- src/server.ts
+++ src/server.ts
@@ -28,4 +28,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(3333, () => console.log("SERVER ONLINE 😁"))
+app.listen(3333, () => console.log("SERVER ONLINE :)😁"))
