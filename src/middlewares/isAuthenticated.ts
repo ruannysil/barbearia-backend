6093d4e7714f1a75f1216306570c6fe766f79fec
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-06-22 22:59:07 -0300

    update backend

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,37 +1,29 @@
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 
 interface Payload {
     sub: string;
 }
 
-export function isAuthenticated(
-    request: Request,
-    response: Response,
-    next: NextFunction
-) {
 
-    const authToken = request.headers.authorization;
+export default function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+    const authToken = req.headers.authorization;
 
     if (!authToken) {
-        return response.status(401).end();
+        return res.status(401).end();
     }
 
-    const [, token] = authToken.split(" ")
+    const [, token] = authToken.split(" ");
 
     try {
 
-        const { sub } = verify(
-            token,
-            process.env.JWT_SECRET
-        ) as Payload;
+        const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
 
-        request.user_id = sub;
+        req.user_id = sub;
 
         return next();
 
     } catch (err) {
-        return response.status(401).end();
+        return res.status(401).end();
     }
-
 }
