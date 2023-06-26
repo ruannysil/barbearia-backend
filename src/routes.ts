f33a151d2f1bb5fb59396a138f92fcd089813deb
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-06-26 19:27:33 -0300

    first commit

--- src/routes.ts
+++ src/routes.ts
@@ -32,7 +32,6 @@ router.get('/', (req: Request, res: Response) => {
 router.post("/users", new CreateUserController().handle)
 router.post("/session", new AuthUserController().handle)
 router.get("/me", isAuthenticated, new DetailUserController().handle)
-// router.get("/me", new DetailUserController().handle);
 router.put("/users", isAuthenticated, new UpdateUserController().handle)
 
 // --- ROTA HAIRCUTS
